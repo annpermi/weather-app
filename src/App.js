a6897f8812b973f63ab8a7003e74a00415c34e0e
annpermi <perminova.anna85@gmail.com>
2021-06-29 13:01:41 -0400

    New date format

--- src/App.js
+++ src/App.js
@@ -19,17 +19,18 @@ function App() {
     }
   }
 
-  const dateBuilder = (d) => {
-    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  // const dateBuilder = (d) => {
+  //   let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+  //   let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
-    let day = days[d.getDay()];
-    let date = d.getDate();
-    let month = months[d.getMonth()];
-    let year = d.getFullYear();
+    // let day = days[d.getDay()];
+    // let date = d.getDate();
+    // let month = months[d.getMonth()];
+    // let year = d.getFullYear();
 
-    return `${day} ${date} ${month} ${year}`
-  }
+    // return `${day} ${date} ${month} ${year}`
+  // }
+
   return (
     <div className={(typeof weather.main != 'undefined') 
     ? ((weather.main.temp > 16) ? 'app warm' : 'app')
@@ -42,7 +43,9 @@ function App() {
           <div>
             <div className="location-box">
               <div className="location">{weather.name}, {weather.sys.country}</div>
-              <div className="date">{dateBuilder(new Date())}</div>
+              {/* <div className="date">{dateBuilder(new Date())}</div> */}
+              {/* new date format */}
+              <div className="date">{String(new window.Date()).slice(3,15)}</div>
             </div>
             <div className="weather-box">
               <div className="temp">
